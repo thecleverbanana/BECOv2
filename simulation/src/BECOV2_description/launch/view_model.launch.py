4d3115e193e2f1309aa478f018322c31dfb25f4e
thecleverbanana <rescuedjerry@gmail.com>
2025-05-19 12:49:06 -0700

    simulation create

---
+++ simulation/src/BECOV2_description/launch/view_model.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    description_path = os.path.join(
+        get_package_share_directory('BECOV2_description'),
+        'urdf',
+        'BECOV2.urdf.xacro'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': Command(['xacro ', description_path])}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', os.path.join(
+                get_package_share_directory('BECOV2_description'),
+                'rviz',
+                'model_preview.rviz'
+            )]
+        )
+    ])
